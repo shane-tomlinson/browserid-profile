aca2aca57442fa4a79e485177c99bca267ae0650
Shane Tomlinson <stomlinson@mozilla.com>
2011-11-15 13:58:09 +0000

    Starting a test site as well as include.js and its tests.

---
+++ resources/src/include.js
@@ -0,0 +1,20 @@
+
+if(!navigator.profile) {
+  navigator.profile = {};
+
+  var jsChannel;
+
+  navigator.profile.get = function(callback, fields) {
+    callback({
+      n: "Shane Tomlinson",
+      photo: "http://1.gravatar.com/avatar/beb82d3a38f7812f94120136a8cfec65"
+    });
+  };
+
+  navigator.profile.init = function(options) {
+    if(options.channel) {
+      jsChannel = options.channel;
+    }
+  }
+}
+
